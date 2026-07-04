6bd641acc11d1d8e858efb17480172740edd4a01
Jano2402 <martinezalejandrouy7@gmail.com>
2026-07-03 23:08:03 -0300

    Learning try catch and exceptions

--- src/Exceptions.java
+++ src/Exceptions.java
@@ -1,5 +1,19 @@
+class NoMoneyException extends RuntimeException {
+    public NoMoneyException(String message) {
+        super(message);
+    }
+}
+
 public class Exceptions {
     public static void main(String[] args) {
-        System.out.println("Exceptions");
+        // try catch
+        try {
+            int i = 3 / 0;
+            System.out.println(i);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        throw new NoMoneyException("You're broke");
     } 
 }
